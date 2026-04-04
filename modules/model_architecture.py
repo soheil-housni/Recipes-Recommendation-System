667ef460fec1cf3c2cbb083a83b5a4825cb09473
Soheil Housni <soheil.housni@gmail.com>
2026-04-04 12:12:51 -0400

    update device

--- modules/model_architecture.py
+++ modules/model_architecture.py
@@ -26,8 +26,11 @@ class RecommendationModel(nn.Module):
 
         self.device=device
 
-        self.hashed_ingredients_ids_encoded_embeddings=hashed_ingredients_ids_encoded_embeddings.to("cpu")
-        self.hashed_recipes_ids_encoded_embeddings=hashed_recipes_ids_encoded_embeddings.to("cpu")
+        #self.hashed_ingredients_ids_encoded_embeddings=hashed_ingredients_ids_encoded_embeddings.to("cpu")
+        #self.hashed_recipes_ids_encoded_embeddings=hashed_recipes_ids_encoded_embeddings.to("cpu")
+
+        self.hashed_ingredients_ids_encoded_embeddings=hashed_ingredients_ids_encoded_embeddings.to(self.device)
+        self.hashed_recipes_ids_encoded_embeddings=hashed_recipes_ids_encoded_embeddings.to(self.device)
 
         self.ingredient_id_emb_dim=ingredient_id_emb_dim
         self.recipe_id_emb_dim=recipe_id_emb_dim
@@ -197,16 +200,19 @@ class RecommendationModel(nn.Module):
                 cls_embeddings=None,
                 mean_embeddings=None
                 ):
-          ingredient_ids_continuous_cpu=ingredient_ids_continuous.clone().cpu().long()
-          encoded_ingredient_ids=self.hashed_ingredients_ids_encoded_embeddings[ingredient_ids_continuous_cpu].to(self.device)
+          #ingredient_ids_continuous_cpu=ingredient_ids_continuous.clone().cpu().long()
+          #encoded_ingredient_ids=self.hashed_ingredients_ids_encoded_embeddings[ingredient_ids_continuous_cpu].to(self.device)
+
+          encoded_ingredient_ids=self.hashed_ingredients_ids_encoded_embeddings[ingredient_ids_continuous].to(self.device)
           encoded_ingredient_ids=self.dhe_fnn_ingredient(encoded_ingredient_ids)
           encoded_ingredients_used=self.weighted_mean_ingredients(ingredient_ids_continuous,encoded_ingredient_ids)
           projected_encoded_ingredients=self.projection_ingredient(encoded_ingredients_used)
           projected_encoded_ingredients=nn.functional.dropout(projected_encoded_ingredients,p=self.projec_dropout,training=self.training)
           projected_encoded_ingredients=self.norm_encoded_ingredients(projected_encoded_ingredients)
           
-          items_cpu=items.clone().cpu().long()
-          encoded_items=self.hashed_recipes_ids_encoded_embeddings[items_cpu].to(self.device)
+          #items_cpu=items.clone().cpu().long()
+          #encoded_items=self.hashed_recipes_ids_encoded_embeddings[items_cpu].to(self.device)
+          encoded_items=self.hashed_recipes_ids_encoded_embeddings[items].to(self.device)
           encoded_items=self.dhe_fnn_items(encoded_items)
           encoded_items_history=self.weighted_mean_items(items,encoded_items,ratings_scaled)
           projected_encoded_items=self.projection_items(encoded_items_history)
